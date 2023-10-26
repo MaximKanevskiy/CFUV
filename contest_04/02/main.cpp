1d770041bccb9e55bfdfd6e941e486d2deef7006
MaximKanevskiy <145599322+MaximKanevskiy@users.noreply.github.com>
2023-10-26 14:08:44 +0300

    Update main.cpp

--- contest_04/02/main.cpp
+++ contest_04/02/main.cpp
@@ -1,1 +1,33 @@
+#include <iostream>
+#include <fstream>
+#include "json.hpp"
 
+using json = nlohmann::json;
+
+int main()
+{
+	std::ifstream file("data.json");
+
+	json jsonFile;
+	file >> jsonFile;
+
+	int user_id;
+	std::cin >> user_id;
+
+    int completed_tasks = 0;
+
+    for (const auto& project : jsonFile)
+    {
+        for (const auto& task : project["tasks"]) 
+        {
+            if (task["user_id"] == user_id && task["completed"] == true) 
+            {
+                completed_tasks++;
+            }
+        }
+    }
+
+    std::cout << completed_tasks;
+
+    return 0;
+}
