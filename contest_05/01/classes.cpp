0983c45530fbe61e6b79fb03f072e8a089791506
MaximKanevskiy <145599322+MaximKanevskiy@users.noreply.github.com>
2023-11-17 10:44:33 +0300

    Create classes.cpp

---
+++ contest_05/01/classes.cpp
@@ -0,0 +1,35 @@
+class Cat
+{
+public:
+    bool isAlive;
+    bool isOpen;
+
+    Cat()
+    {
+        this->isAlive = rand() % 2;
+        this->isOpen = false;
+    }
+
+    bool is_alive()
+    {
+        if (this->isOpen)
+        {
+            return this->isAlive;
+        }
+        else
+        {
+            return rand() % 2;
+        }
+    }
+};
+
+class Box
+{
+public:
+    Cat open()
+    {
+        Cat cat;
+        cat.isOpen = true;
+        return cat;
+    }
+};
